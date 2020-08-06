b2ab6a399172a084e1ddb847df4485754244c56e
grant <ligaolong@aliyun.com>
2020-08-06 11:24:44 +0800

    [version]:1.0.0 [修改内容]: [新增功能]:first commit springboot project [Note]:

---
+++ src/test/java/com/grant/research/ResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grant.research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
